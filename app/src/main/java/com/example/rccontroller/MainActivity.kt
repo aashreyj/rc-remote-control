8668f6a3339a141e218413639f7d6345512e0f0e
Aashrey <aashrey9.jain@gmail.com>
2020-01-22 01:03:02 +0530

    bug fix

--- app/src/main/java/com/example/rccontroller/MainActivity.kt
+++ app/src/main/java/com/example/rccontroller/MainActivity.kt
@@ -14,8 +14,6 @@ import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    val RequestQueue = Volley.newRequestQueue(this)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,40 +25,41 @@ class MainActivity : AppCompatActivity() {
         val startButton: Button = findViewById(R.id.startButton)
         val ipAddress: EditText = findViewById(R.id.ipAddress)
         val url = "http://" + ipAddress.text.toString() + "/"
+        val requestQueue = Volley.newRequestQueue(this)
 
         startButton.setOnClickListener { Toast.makeText(this, "Ready to Control!",Toast.LENGTH_SHORT).show() }
 
         forwardButton.setOnClickListener {
             val forwardObject = JSONObject()
             forwardObject.put("State", "F")
-            sendGetRequest(url, forwardObject)
+            sendGetRequest(url, forwardObject, requestQueue)
         }
 
         backwardButton.setOnClickListener {
             val backwardObject = JSONObject()
             backwardObject.put("State", "B")
-            sendGetRequest(url, backwardObject)
+            sendGetRequest(url, backwardObject, requestQueue)
         }
 
         rightButton.setOnClickListener {
             val rightObject = JSONObject()
             rightObject.put("State", "R")
-            sendGetRequest(url, rightObject)
+            sendGetRequest(url, rightObject, requestQueue)
         }
 
         leftButton.setOnClickListener {
             val leftObject = JSONObject()
             leftObject.put("State", "L")
-            sendGetRequest(url, leftObject)
+            sendGetRequest(url, leftObject, requestQueue)
         }
     }
 
-    fun sendGetRequest(url: String, requestObject: JSONObject)
+    fun sendGetRequest(url: String, requestObject: JSONObject, requestQueue: RequestQueue)
     {
         val request = JsonObjectRequest(Request.Method.GET, url, requestObject,
             Response.Listener { response -> Toast.makeText(this, response.toString(), Toast.LENGTH_SHORT).show()},
             Response.ErrorListener {Toast.makeText(this, "There was an Error!", Toast.LENGTH_SHORT).show()})
 
-        RequestQueue.add(request)
+        requestQueue.add(request)
     }
 }
