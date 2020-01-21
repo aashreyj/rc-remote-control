b15fd27a87415565386cf594a6bb07f93691386e
Aashrey <aashrey9.jain@gmail.com>
2020-01-22 00:34:49 +0530

    ui complete

--- app/src/main/java/com/example/rccontroller/MainActivity.kt
+++ app/src/main/java/com/example/rccontroller/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.rccontroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    val RequestQueue = Volley.newRequestQueue(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val forwardButton: Button = findViewById(R.id.upArrow)
+        val backwardButton: Button = findViewById(R.id.downArrow)
+        val rightButton: Button = findViewById(R.id.rightArrow)
+        val leftButton: Button = findViewById(R.id.leftArrow)
     }
+
+    
 }
